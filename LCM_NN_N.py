55b1702835db1c376568dfb08ea99ea2b2953e93
Mikhail Shutenkov <shutenkov.m@gmail.com>
2018-03-29 13:31:46 +0300

    add ABS_Z_N, LCM_NN_N and TRANS_Z_N functions

---
+++ LCM_NN_N.py
@@ -0,0 +1,13 @@
+import DIV_NN_N
+import GCF_NN_N
+
+def LCM_NN_N(a,b):
+    """
+    N-14
+    НОК двух натуральных чисел
+    Копылов Петр, 7305
+    """
+
+    # НОК(A, B) = A * B / НОД(A, B)
+    return DIV_NN_N(MUL_NN_N(a, b) , GCF_NN_N(a,b))
+
